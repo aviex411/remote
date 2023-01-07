39015d1bd87406dfe3a80b04cbe9470c86dcdc08
Aviex <108202612+aviex411@users.noreply.github.com>
2023-01-07 20:07:31 +0700

    Create stat.js

---
+++ stat.js
@@ -0,0 +1,22 @@
+<!-- Default Statcounter code for NodeJs -->
+<script type="text/javascript">
+  var sc_project = 12834158;
+  var sc_invisible = 1;
+  var sc_security = "912ed129";
+</script>
+<script
+  type="text/javascript"
+  src="https://www.statcounter.com/counter/counter.js"
+  async
+></script>
+<noscript
+  ><div class="statcounter">
+    <a title="Web Analytics" href="https://statcounter.com/" target="_blank"
+      ><img
+        class="statcounter"
+        src="https://c.statcounter.com/12834158/0/912ed129/1/"
+        alt="Web Analytics"
+        referrerpolicy="no-referrer-when-downgrade"
+    /></a></div
+></noscript>
+<!-- End of Statcounter Code -->
