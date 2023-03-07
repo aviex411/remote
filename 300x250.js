83f01839a08980e785e5da67e18f3e1bc4405539
Aviex <108202612+aviex411@users.noreply.github.com>
2023-03-07 22:59:57 +0700

    Create 300x250.js

---
+++ 300x250.js
@@ -0,0 +1,8 @@
+atOptions = {
+  'key' : 'cf2e6d1d7610db7519353beb4b314861',
+  'format' : 'iframe',
+  'height' : 250,
+  'width' : 300,
+  'params' : {}
+};
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://preparemethod.com/cf2e6d1d7610db7519353beb4b314861/invoke.js"></scr' + 'ipt>');
