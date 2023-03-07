37c45bae90a0dadcbb247818daf3cbbd18f02486
Aviex <108202612+aviex411@users.noreply.github.com>
2023-03-07 22:27:19 +0700

    Create 468x60.js

---
+++ 468x60.js
@@ -0,0 +1,8 @@
+atOptions = {
+  'key' : '04b1f5cec53a32921e73b15c36a52fb6',
+  'format' : 'iframe',
+  'height' : 60,
+  'width' : 468,
+  'params' : {}
+};
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://preparemethod.com/04b1f5cec53a32921e73b15c36a52fb6/invoke.js"></scr' + 'ipt>');
