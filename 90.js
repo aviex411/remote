cc4ffcfad87bafde5050cf48e93caebc95c22c36
Aviex <108202612+aviex411@users.noreply.github.com>
2023-03-07 22:19:59 +0700

    Delete 90.js

--- 90.js
+++
@@ -1,8 +0,0 @@
-atOptions = {
-  'key' : 'f77bf6af7ceb4da1a2eeaafb7ca85fd9',
-  'format' : 'iframe',
-  'height' : 90,
-  'width' : 728,
-  'params' : {}
-};
-preparemethod.com/f77bf6af7ceb4da1a2eeaafb7ca85fd9/invoke.js
