b815f175f570ad77ceee33006f356a75ea181f53
Aviex <108202612+aviex411@users.noreply.github.com>
2023-03-07 22:22:06 +0700

    Create 728x90.js

---
+++ 728x90.js
@@ -0,0 +1,8 @@
+atOptions = {
+    'key' : 'f77bf6af7ceb4da1a2eeaafb7ca85fd9',
+    'format' : 'iframe',
+    'height' : 90,
+    'width' : 728,
+    'params' : {}
+};
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://preparemethod.com/f77bf6af7ceb4da1a2eeaafb7ca85fd9/invoke.js"></scr' + 'ipt>');
